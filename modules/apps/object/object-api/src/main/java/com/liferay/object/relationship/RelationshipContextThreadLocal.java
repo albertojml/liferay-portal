a082fa274c3dcb7411832529af8897dd507d8141
Alberto Moreno <alberto.moreno@liferay.com>
2024-10-25 09:12:39 +0200

    LPD-37768 Modify previous validation threadlocal to support the new needed parameters

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/relationship/RelationshipContextThreadLocal.java
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.relationship;
+
+import com.liferay.petra.lang.CentralizedThreadLocal;
+
+/**
+ * @author Alberto Javier Moreno Lage
+ */
+public class RelationshipContextThreadLocal {
+
+	public static RelationshipContext getAndSetRelationshipContext(
+		RelationshipContext relationshipContext) {
+
+		RelationshipContext oldRelationshipContext = getNestedRelationshipContext();
+
+		setRelationshipContextThreadLocal(relationshipContext);
+
+		return oldRelationshipContext;
+	}
+
+	public static RelationshipContext getNestedRelationshipContext() {
+		return _relationshipContextThreadLocal.get();
+	}
+
+	public static void setRelationshipContextThreadLocal(
+		RelationshipContext relationshipContext) {
+
+		_relationshipContextThreadLocal.set(relationshipContext);
+	}
+
+	private static final CentralizedThreadLocal<RelationshipContext>
+		_relationshipContextThreadLocal =
+			new CentralizedThreadLocal<>(
+				RelationshipContextThreadLocal.class +
+				"._relationshipContext");
+}
