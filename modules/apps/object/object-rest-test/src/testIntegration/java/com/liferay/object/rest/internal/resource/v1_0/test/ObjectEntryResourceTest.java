13471336bc6e0306c2c21485c7e13bca6848ea7a
Alberto Moreno <alberto.moreno@liferay.com>
2025-05-29 09:55:03 +0200

    LPD-55622 Rename

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -9233,7 +9233,7 @@ public class ObjectEntryResourceTest {
 	}
 
 	@Test
-	public void testPostNestedSiteObjectEntryThroughCompanyObject()
+	public void testPostNestedSiteObjectEntryThroughCompanyToSiteScopedRelationship()
 		throws Exception {
 
 		_objectRelationship1 = ObjectRelationshipTestUtil.addObjectRelationship(
