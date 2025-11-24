55e4d82c72bc7db7daee2fa13df768de106f99c1
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-11-24 15:15:58 -0300

    LPD-69419 implement new delete comment endpoint

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
@@ -256,6 +256,34 @@ public class ObjectEntryResourceImpl
 	}
 
 	@Override
+	public void
+			deleteByExternalReferenceCodeObjectEntryExternalReferenceCodeCommentByExternalReferenceCode(
+				String objectEntryExternalReferenceCode,
+				String externalReferenceCode)
+		throws Exception {
+
+		if (!_objectDefinition.isEnableComments() ||
+			!FeatureFlagManagerUtil.isEnabled(
+				_objectDefinition.getCompanyId(), "LPD-69419")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		ObjectEntry objectEntry = _getObjectEntry(
+			objectEntryExternalReferenceCode, null);
+
+		com.liferay.portal.kernel.comment.Comment comment = _fetchComment(
+			ObjectEntry.class.getName(), objectEntry.getId(),
+			externalReferenceCode, _getNonzeroGroupId(objectEntry.getId()));
+
+		if (comment == null) {
+			throw new NotFoundException();
+		}
+
+		_deleteComment(comment.getCommentId());
+	}
+
+	@Override
 	public void deleteObjectEntry(Long objectEntryId) throws Exception {
 		DefaultObjectEntryManager defaultObjectEntryManager =
 			DefaultObjectEntryManagerProvider.provide(
@@ -323,6 +351,34 @@ public class ObjectEntryResourceImpl
 
 		defaultObjectEntryManager.deleteObjectEntryByVersion(
 			externalReferenceCode, _objectDefinition, scopeKey, version);
+	}
+
+	@Override
+	public void
+			deleteScopeScopeKeyByExternalReferenceCodeObjectEntryExternalReferenceCodeCommentByExternalReferenceCode(
+				String scopeKey, String objectEntryExternalReferenceCode,
+				String externalReferenceCode)
+		throws Exception {
+
+		if (!_objectDefinition.isEnableComments() ||
+			!FeatureFlagManagerUtil.isEnabled(
+				_objectDefinition.getCompanyId(), "LPD-69419")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		ObjectEntry objectEntry = _getObjectEntry(
+			objectEntryExternalReferenceCode, scopeKey);
+
+		com.liferay.portal.kernel.comment.Comment comment = _fetchComment(
+			ObjectEntry.class.getName(), objectEntry.getId(),
+			externalReferenceCode, objectEntry.getScopeId());
+
+		if (comment == null) {
+			throw new NotFoundException();
+		}
+
+		_deleteComment(comment.getCommentId());
 	}
 
 	@Override
@@ -1540,6 +1596,13 @@ public class ObjectEntryResourceImpl
 
 			return serviceContext;
 		};
+	}
+
+	private void _deleteComment(Long commentId) throws Exception {
+		_discussionPermission.checkDeletePermission(
+			PermissionThreadLocal.getPermissionChecker(), commentId);
+
+		_commentManager.deleteComment(commentId);
 	}
 
 	private com.liferay.portal.kernel.comment.Comment _fetchComment(
