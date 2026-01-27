f42cf5729102ad0338d2c389aacf5296a8e9d54b
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-01-27 08:57:57 +0100

    LPD-72013 Enhance the site page asserts

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
@@ -2580,12 +2580,17 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 
 		Assert.assertEquals(WorkflowConstants.STATUS_EMPTY, layout.getStatus());
 
-		sitePageResource.putSiteSitePage(
+		SitePage putSitePage = sitePageResource.putSiteSitePage(
 			testGroup.getExternalReferenceCode(),
 			sitePage.getExternalReferenceCode(), sitePage);
 
+		assertEquals(sitePage, putSitePage);
+		assertValid(putSitePage);
+
 		layout = _layoutLocalService.getLayoutByExternalReferenceCode(
 			sitePage.getExternalReferenceCode(), testGroup.getGroupId());
+
+		_assertSitePage(layout, putSitePage);
 
 		Assert.assertEquals(
 			WorkflowConstants.STATUS_APPROVED, layout.getStatus());
@@ -2629,12 +2634,17 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 
 		Assert.assertEquals(WorkflowConstants.STATUS_EMPTY, layout.getStatus());
 
-		sitePageResource.putSiteSitePage(
+		SitePage putSitePage = sitePageResource.putSiteSitePage(
 			testGroup.getExternalReferenceCode(),
 			sitePage.getExternalReferenceCode(), sitePage);
 
+		assertEquals(sitePage, putSitePage);
+		assertValid(putSitePage);
+
 		layout = _layoutLocalService.getLayoutByExternalReferenceCode(
 			sitePage.getExternalReferenceCode(), testGroup.getGroupId());
+
+		_assertSitePage(layout, putSitePage);
 
 		Assert.assertEquals(
 			WorkflowConstants.STATUS_APPROVED, layout.getStatus());
