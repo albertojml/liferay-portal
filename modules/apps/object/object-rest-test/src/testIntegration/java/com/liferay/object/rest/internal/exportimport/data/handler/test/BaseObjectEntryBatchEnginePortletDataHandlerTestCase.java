d229eb9db8d2fb32ffe06e60e8a69233c00aff9e
Alberto Moreno <alberto.moreno@liferay.com>
2026-07-14 09:56:34 +0200

    LPD-97697 Verify ObjectEntry batch export-import compliance
    Run the batch compliance test base against site and depot scoped object definitions with comments enabled. The site and depot concrete classes share an abstract ObjectEntry test case with a definition scope hook because the Arquillian bridge does not support parameterized runners. Depot definitions accept all groups so entries can be added to the asset libraries, comments for non site scopes require the LPD-43996 feature flag, and entry listings filter by approved status because depot deletions move entries to the recycle bin (LPD-17564) instead of removing them. The delegate is resolved from the OSGi registry through the ObjectEntryResource registration, entries pin their modified date through the persistence layer's hasSetModifiedDate handling, and the module classpath gains the APIs the base references because the Arquillian client JVM also loads the test class locally.

---
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/exportimport/data/handler/test/BaseObjectEntryBatchEnginePortletDataHandlerTestCase.java
@@ -0,0 +1,236 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.rest.internal.exportimport.data.handler.test;
+
+import com.liferay.exportimport.test.util.exportimport.data.handler.BaseBatchEnginePortletDataHandlerTestCase;
+import com.liferay.exportimport.vulcan.batch.engine.ExportImportVulcanBatchEngineTaskItemDelegate;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectDefinitionSettingConstants;
+import com.liferay.object.constants.ObjectEntryFolderConstants;
+import com.liferay.object.definition.setting.builder.ObjectDefinitionSettingBuilder;
+import com.liferay.object.field.builder.TextObjectFieldBuilder;
+import com.liferay.object.field.util.ObjectFieldUtil;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectDefinitionSetting;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.rest.resource.v1_0.ObjectEntryResource;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.test.util.ObjectDefinitionTestUtil;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author Alberto Javier Moreno Lage
+ */
+public abstract class BaseObjectEntryBatchEnginePortletDataHandlerTestCase
+	extends BaseBatchEnginePortletDataHandlerTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		List<ObjectDefinitionSetting> objectDefinitionSettings =
+			Collections.emptyList();
+
+		String scope = getObjectDefinitionScope();
+
+		if (StringUtil.equals(scope, ObjectDefinitionConstants.SCOPE_DEPOT)) {
+			objectDefinitionSettings = Collections.singletonList(
+				new ObjectDefinitionSettingBuilder(
+				).name(
+					ObjectDefinitionSettingConstants.NAME_ACCEPT_ALL_GROUPS
+				).value(
+					StringPool.TRUE
+				).build());
+		}
+
+		_objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				null, TestPropsValues.getUserId(), 0, null, true, true, false,
+				false, true, false, false, false, false, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				ObjectDefinitionTestUtil.getRandomName(), null, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				true, scope, ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+				objectDefinitionSettings, Collections.emptyList(),
+				Collections.emptyList(), new ServiceContext());
+
+		ObjectField objectField = ObjectFieldUtil.addCustomObjectField(
+			new TextObjectFieldBuilder(
+			).userId(
+				TestPropsValues.getUserId()
+			).labelMap(
+				LocalizedMapUtil.getLocalizedMap("able")
+			).name(
+				"able"
+			).objectDefinitionId(
+				_objectDefinition.getObjectDefinitionId()
+			).required(
+				false
+			).build());
+
+		_objectDefinitionLocalService.updateTitleObjectFieldId(
+			_objectDefinition.getObjectDefinitionId(),
+			objectField.getObjectFieldId());
+
+		_objectDefinition =
+			_objectDefinitionLocalService.publishCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				_objectDefinition.getObjectDefinitionId());
+
+		super.setUp();
+	}
+
+	@Override
+	protected String addEntry(long groupId, long userId, Date dateModified)
+		throws Exception {
+
+		ObjectEntry objectEntry = _objectEntryLocalService.addObjectEntry(
+			groupId, userId, _objectDefinition.getObjectDefinitionId(),
+			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
+			null,
+			Collections.singletonMap("able", RandomTestUtil.randomString()),
+			ServiceContextTestUtil.getServiceContext(groupId, userId));
+
+		objectEntry.setModifiedDate(dateModified);
+
+		objectEntry = _objectEntryLocalService.updateObjectEntry(objectEntry);
+
+		return objectEntry.getExternalReferenceCode();
+	}
+
+	@Override
+	protected void addStagedModels() throws Exception {
+	}
+
+	@Override
+	protected void deleteEntry(long groupId, String externalReferenceCode)
+		throws Exception {
+
+		_objectEntryLocalService.deleteObjectEntry(
+			_getObjectEntry(groupId, externalReferenceCode));
+	}
+
+	@Override
+	protected long getCreatorUserId(long groupId, String externalReferenceCode)
+		throws Exception {
+
+		ObjectEntry objectEntry = _getObjectEntry(
+			groupId, externalReferenceCode);
+
+		return objectEntry.getUserId();
+	}
+
+	@Override
+	protected List<String> getExpectedNestedFields() {
+		return Collections.singletonList("rootModelHierarchy");
+	}
+
+	@Override
+	protected ExportImportVulcanBatchEngineTaskItemDelegate<?>
+		getExportImportVulcanBatchEngineTaskItemDelegate() {
+
+		try {
+			Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+			BundleContext bundleContext = bundle.getBundleContext();
+
+			Collection<ServiceReference<ObjectEntryResource>>
+				serviceReferences = bundleContext.getServiceReferences(
+					ObjectEntryResource.class,
+					StringBundler.concat(
+						"(&(batch.engine.task.item.delegate.name=",
+						_objectDefinition.getName(), ")(companyId=",
+						_objectDefinition.getCompanyId(), "))"));
+
+			Iterator<ServiceReference<ObjectEntryResource>> iterator =
+				serviceReferences.iterator();
+
+			return (ExportImportVulcanBatchEngineTaskItemDelegate<?>)
+				bundleContext.getService(iterator.next());
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	@Override
+	protected List<String> getExternalReferenceCodes(long groupId)
+		throws Exception {
+
+		return TransformUtil.transform(
+			_objectEntryLocalService.getObjectEntries(
+				groupId, _objectDefinition.getObjectDefinitionId(),
+				WorkflowConstants.STATUS_APPROVED, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS),
+			ObjectEntry::getExternalReferenceCode);
+	}
+
+	protected abstract String getObjectDefinitionScope();
+
+	@Override
+	protected String getPortletId() {
+		return _objectDefinition.getPortletId();
+	}
+
+	@Override
+	protected long getPrimaryKey(long groupId, String externalReferenceCode)
+		throws Exception {
+
+		ObjectEntry objectEntry = _getObjectEntry(
+			groupId, externalReferenceCode);
+
+		return objectEntry.getObjectEntryId();
+	}
+
+	@Override
+	protected boolean supportsComments() {
+		return true;
+	}
+
+	private ObjectEntry _getObjectEntry(
+		long groupId, String externalReferenceCode) {
+
+		return _objectEntryLocalService.fetchObjectEntry(
+			externalReferenceCode, groupId,
+			_objectDefinition.getObjectDefinitionId());
+	}
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _objectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+}
