abaa8c6cb00222c5b41ef49efc07e26e9a79474a
Alberto Moreno <alberto.moreno@liferay.com>
2026-08-12 13:35:19 +0200

    LPD-102325 Complete the sentence in the missing part message

--- modules/apps/mcp/mcp-server-rest-impl/src/test/java/com/liferay/mcp/server/rest/internal/util/OpenAPIUtilTest.java
+++ modules/apps/mcp/mcp-server-rest-impl/src/test/java/com/liferay/mcp/server/rest/internal/util/OpenAPIUtilTest.java
@@ -573,7 +573,7 @@ public class OpenAPIUtilTest {
 
 		throw new IllegalArgumentException(
 			StringBundler.concat(
-				"No part named \"", fieldName, "\" in ", fileItems));
+				"No part named \"", fieldName, "\" was found in ", fileItems));
 	}
 
 	private List<FileItem> _getFileItems(VulcanRequestForwarder.Request request)
